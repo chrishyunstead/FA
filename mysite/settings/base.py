be15ae1d64e921d9991a11b788365f37ea3e1dfc
HanW001-S <115495189+HanW001-S@users.noreply.github.com>
2024-06-26 13:03:34 +0900

    Update base.py

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -194,9 +194,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "/static")
-STATICFILES_DIRS = [
-    BASE_DIR / "core" / "src-django-components",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "core" / "src-django-components",
+# ]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
